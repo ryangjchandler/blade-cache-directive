f8d08574dc227a7b1144d855b2de3179a8b2c688
Ryan Chandler <ryangjchandler@gmail.com>
2021-05-22 14:54:56 +0100

    feature: basic cache directive

--- src/BladeCacheDirectiveServiceProvider.php
+++ src/BladeCacheDirectiveServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace RyanChandler\BladeCacheDirective;
 
+use Illuminate\Support\Facades\Blade;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use RyanChandler\BladeCacheDirective\Commands\BladeCacheDirectiveCommand;
@@ -12,7 +13,48 @@ class BladeCacheDirectiveServiceProvider extends PackageServiceProvider
     {
         $package
             ->name('blade-cache-directive')
-            ->hasConfigFile()
-            ->hasViews();
+            ->hasConfigFile();
+    }
+
+    public function packageBooted()
+    {
+        Blade::directive('cache', function ($expression) {
+            return "<?php
+                \$__cache_directive_arguments = [{$expression}];
+
+                if (count(\$__cache_directive_arguments) === 2) {
+                    [\$__cache_directive_key, \$__cache_directive_ttl] = \$__cache_directive_arguments;
+                } else {
+                    [\$__cache_directive_key] = \$__cache_directive_arguments;
+                    \$__cache_directive_ttl = config('blade-cache-directive.ttl');
+                }
+
+                if (\Illuminate\Support\Facades\Cache::has(\$__cache_directive_key)) {
+                    echo \Illuminate\Support\Facades\Cache::get(\$__cache_directive_key);
+
+                    ob_start();
+                } else {
+                    \$__cache_directive_buffering = true;
+
+                    ob_start();
+                }
+            ?>";
+        });
+
+        Blade::directive('endcache', function () {
+            return "<?php
+                if (isset(\$__cache_directive_buffering)) {
+                    \$__cache_directive_buffer = ob_get_clean();
+
+                    \Illuminate\Support\Facades\Cache::put(\$__cache_directive_key, \$__cache_directive_buffer, \$__cache_directive_ttl);
+
+                    echo \$__cache_directive_buffer;
+
+                    unset(\$__cache_directive_key, \$__cache_directive_ttl, \$__cache_directive_buffering, \$__cache_directive_buffering, \$__cache_directive_arguments);
+                } else {
+                    ob_end_clean();
+                }
+            ?>";
+        });
     }
 }

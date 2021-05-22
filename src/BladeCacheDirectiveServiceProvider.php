87f83ad14cc5c620c82edfb8d48e3e43fc96e266
Ryan Chandler <ryangjchandler@gmail.com>
2021-05-22 21:51:11 +0100

    fix: dont evaluate contents twice

--- src/BladeCacheDirectiveServiceProvider.php
+++ src/BladeCacheDirectiveServiceProvider.php
@@ -30,28 +30,22 @@ class BladeCacheDirectiveServiceProvider extends PackageServiceProvider
 
                 if (\Illuminate\Support\Facades\Cache::has(\$__cache_directive_key)) {
                     echo \Illuminate\Support\Facades\Cache::get(\$__cache_directive_key);
-
-                    ob_start();
                 } else {
                     \$__cache_directive_buffering = true;
 
                     ob_start();
-                }
             ?>";
         });
 
         Blade::directive('endcache', function () {
             return "<?php
-                if (isset(\$__cache_directive_buffering)) {
                     \$__cache_directive_buffer = ob_get_clean();
 
                     \Illuminate\Support\Facades\Cache::put(\$__cache_directive_key, \$__cache_directive_buffer, \$__cache_directive_ttl);
 
                     echo \$__cache_directive_buffer;
 
-                    unset(\$__cache_directive_key, \$__cache_directive_ttl, \$__cache_directive_buffering, \$__cache_directive_buffering, \$__cache_directive_arguments);
-                } else {
-                    ob_end_clean();
+                    unset(\$__cache_directive_key, \$__cache_directive_ttl, \$__cache_directive_buffer, \$__cache_directive_buffering, \$__cache_directive_arguments);
                 }
             ?>";
         });
